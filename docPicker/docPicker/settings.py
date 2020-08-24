4c89f55921dd985d1724262ee0cce13a18c65de8
AlphaAl <rahul.tripathi7293@gmail.com>
2020-08-24 21:35:29 +0530

    just testing stuff

--- docPicker/docPicker/settings.py
+++ docPicker/docPicker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tDetection.apps.TdetectionConfig',
+    'rest-framework',
 ]
 
 MIDDLEWARE = [
--- docPicker/tDetection/views.py
+++ docPicker/tDetection/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
+def normal(request):
+	now = datetime.datetime.now()
+	html = "<html><p>this is now %s.</p></html>" % now 
+	return HttpResponse(html)
 # Create your views here.
